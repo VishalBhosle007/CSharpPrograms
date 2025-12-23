0da26890f01b4e8b9fc2f3984a4d78fc850e8953
Vishal Bhosle <vishalbhosle1432@gmail.com>
2025-12-23 17:19:12 +0530

    Sum of digits of a number

---
+++ Programs/CSharpPrograms/SumOfDigits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms
+{
+    public class Sumofdigits : IProgram
+    {
+        public string Name => "Sum of digits of a number";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter number to get sum of digits");
+            int? num = null;
+            num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10 ?? 0;
+                sum += digit;
+                num = num / 10;
+            }
+            Console.WriteLine($"Sum of digits is {sum}");
+        }
+    }
+}
