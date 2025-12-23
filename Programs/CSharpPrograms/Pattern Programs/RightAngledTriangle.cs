e4b39957d5d2a2e11c81876fca31e5204e3bb8ad
Vishal Bhosle <vishalbhosle1432@gmail.com>
2025-12-23 17:23:47 +0530

    Print Right-angled triangle

---
+++ Programs/CSharpPrograms/Pattern Programs/RightAngledTriangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.Pattern_Programs
+{
+    public class RightAngledTriangle : IProgram
+    {
+        public string Name => "Print Right-angled triangle";
+
+        public void Run()
+        {
+            int n = 5;
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
