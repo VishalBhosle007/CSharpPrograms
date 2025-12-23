a6a5298e6144acdb8c8c24355ccc2f7c306e83db
Vishal Bhosle <vishalbhosle1432@gmail.com>
2025-12-23 18:49:35 +0530

    Count occurrences of each character in a string

---
+++ Programs/CSharpPrograms/String Programs/CountOccurrencesOfCharacterInString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.String_Programs
+{
+    public class CountOccurrencesOfCharacterInString : IProgram
+    {
+        public string Name => "Count occurrences of each character in a string";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter string to count occurrences of each character");
+            string str = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(str))
+                return;
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (char c in str.ToLower())
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c]++;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+
+            foreach (var ele in dict)
+            {
+                Console.WriteLine($"{ele.Key} = {ele.Value}");
+            }
+        }
+    }
+}
