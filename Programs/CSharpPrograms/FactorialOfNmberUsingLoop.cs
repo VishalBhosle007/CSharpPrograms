a07fbe527260416bd1292bb2938e6eeec246d704
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-10 21:25:27 +0530

    Factorial of a number using loop

---
+++ Programs/CSharpPrograms/FactorialOfNmberUsingLoop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms
+{
+    public class FactorialOfNmberUsingLoop : IProgram
+    {
+        public string Name => "Factorial of a number using loop";
+        public void Run()
+        {
+            Console.WriteLine("Enter a number to calculate its factorial:");
+            int n = int.Parse(Console.ReadLine());
+            
+            long factorial = 1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"The factorial of {n} is {factorial}.");
+        }
+    
+    }
+}
