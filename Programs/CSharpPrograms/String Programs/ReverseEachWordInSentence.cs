e71feb467367b9e96f017256f5960fd7332eb833
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-15 15:03:20 +0530

    Reverse each word in a sentence

---
+++ Programs/CSharpPrograms/String Programs/ReverseEachWordInSentence.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.String_Programs
+{
+    public class ReverseEachWordInSentence : IProgram
+    {
+        public string Name => "Reverse each word in sentence";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter string to reverse each word");
+            string str = Console.ReadLine();
+
+            StringBuilder revStr = new StringBuilder();
+            StringBuilder word = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] != ' ')
+                {
+                    word.Append(str[i]);
+                }
+                else
+                {
+                    for (int j = word.Length-1; j >= 0; j--)
+                    {
+                        revStr.Append(word[j]);
+                    }
+                    revStr.Append(' ');
+                    word.Clear();
+                }
+
+            }
+
+            // Reverse last word
+            for (int j = word.Length-1; j >= 0; j--)
+            {
+                revStr.Append(word[j]);
+            }
+
+
+            Console.WriteLine(revStr.ToString());
+
+        }
+    }
+}
