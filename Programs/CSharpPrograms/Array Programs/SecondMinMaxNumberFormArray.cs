cbd8da3663f2b140f47a2543cd087efd86a3fb7c
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-18 19:43:14 +0530

    Find second largest and second smallest element in an array

---
+++ Programs/CSharpPrograms/Array Programs/SecondMinMaxNumberFormArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.Array_Programs
+{
+    public class SecondMinMaxNumberFormArray : IProgram
+    {
+        public string Name => "Find second largest and second smallest element in an array";
+
+        public void Run()
+        {
+            int[] arr = { 1, 2, 3, 5, 6, 7, 8, 9 };
+
+            int min = int.MaxValue;
+            int secondMin = int.MaxValue;
+            int max = int.MinValue;
+            int secondMax = int.MinValue;
+
+            foreach (int num in arr)
+            {
+                //Min
+                if (num < min)
+                {
+                    secondMin = min;
+                    min = num;
+                }
+                else if (num > min && num < secondMin)
+                {
+                    secondMin = num;
+                }
+
+                //Max
+                if (num > max)
+                {
+                    secondMax = max;
+                    max = num;
+                }
+                else if (num < max && num > secondMax)
+                {
+                    secondMax = num;
+                }
+
+            }
+
+            Console.WriteLine($"Min = {min}, Max = {max}");
+            Console.WriteLine($"Second Min = {secondMin}, Second Max = {secondMax}");
+        }
+    }
+}
